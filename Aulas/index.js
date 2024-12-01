e0cb88127f6df23e6badceb0c6e1bc0e75452a55
wesley_rody <wesley_rody@yahoo.com.br>
2024-12-01 20:06:10 -0300

    Finalizado

--- Aulas/index.js
+++ Aulas/index.js
@@ -1,3 +1,26 @@
-let nome = "Wesley Lima";
-console.log("Olá amigos");
-console.log("atualizado");
+let nome = "Wesley";
+let xp = 20000;
+
+function determinarLiga(xp) {
+    if (xp <= 1000) {
+        return "Ferro";
+    } else if (xp <= 2000) {
+        return "Bronze";
+    } else if (xp <= 5000) {
+        return "Prata";
+    } else if (xp <= 7000) {
+        return "Ouro";
+    } else if (xp <= 8000) {
+        return "Platina";
+    } else if (xp <= 9000) {
+        return "Ascendente";
+    } else if (xp <= 10000) {
+        return "Imortal";
+    } else {
+        return "Radiante";
+    }
+}
+
+let nivel = determinarLiga(xp);
+
+console.log("O Herói " + nome + " está no nível de " + nivel); 
